83cb0a8c3c991b8a43b3e2ad2d2d0f6d14ab390e
Josh Kleinschmidt <lalaman744@hotmail.com>
2019-03-19 18:07:34 -0500

    Initial commit added 3 view controllers and set up segues

---
+++ Programmer Resources/Resource.swift
@@ -0,0 +1,9 @@
+//
+//  Resource.swift
+//  Programmer Resources
+//
+//  Created by Josh Kleinschmidt on 3/19/19.
+//  Copyright © 2019 Josh Kleinschmidt. All rights reserved.
+//
+
+import Foundation
